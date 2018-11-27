a56259dddf5af002becde516decb7f21f1194bd4
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-27 20:36:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setGame(prefix + "Hehehe");
+    console.log('I am ready!')
 });
 
 client.on('message', message => {
