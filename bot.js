0bc9bcf8d6f59801b9f8cdf0d623a25d8264aa62
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-28 20:45:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const bot = new Discord.Client();
 
 bot.on('ready', () => {
   bot.user.setGame('GEJM HERE')
+  console.log('I am ready!');
 })
   
-
 client.on('message', message => {
     if (message.content === 'vert') {
     	message.reply('Penisek');
   	}
-
 });
 
 // THIS  MUST  BE  THIS  WAY
