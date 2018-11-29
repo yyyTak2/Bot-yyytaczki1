87df4a8005e145550bca5b7b4c75208f561a6170
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 12:53:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-    
-client.user.setActivity('YouTube', { type: 'WATCHING' });
-    
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.B0T_T0KEN);
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+bot.on("ready", () => {
+  console.log("Ready");
+  bot.user.setActivity("Type !help");
+})
