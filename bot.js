f4b700dbad0a125da0ce1dbadc5b1865382104de
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 14:52:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,18 @@ bot.on('ready', () => {
 });
 
 client.on('message', message => {
-else if(message.content.toLowerCase() === 'lol')
+
+ if(message.content.toLowerCase() === 'hello')
+      message.channel.send('lololol' + message.author);
+   
+ else if(message.content.toLowerCase() === 'lol')
+      message.reply('pong');
+
+  else if(message.content.toLowerCase() === 'lol')
       message.channel.send('eksdi');
+   
+});
+   
    
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.B0T_T0KEN);
