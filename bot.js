9737d168bcb02d0849fef297f7f4ee637ba0e76c
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 22:29:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
-var client = new Discord.Client();
 
 bot.on('ready', () => {
     bot.user.setStatus('dnd')
@@ -9,17 +8,14 @@ bot.on('ready', () => {
             name: '🔥Łazienka Kamilka🔥 👉👌',
             type: "STREAMING",
             url: "https://www.twitch.tv/yyytak32"
-
         }
     });
 });
 
 bot.on('message', message => {
-
-  if(message.content.toLowerCase() === 'twiks')
-      message.channel.send('Przyjdę do cb zaraz i zgwałce ci dom ❤️');
+if(message.content.toLowerCase() === 'twiks')
+   message.channel.send('Przyjdę do cb zaraz i zgwałce ci dom ❤️');
     
 });
-   
-// THIS  MUST  BE  THIS  WAY
+
 bot.login(process.env.B0T_T0KEN);
