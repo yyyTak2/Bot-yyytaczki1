e51acb0aebcd7fbee87f797217b91a2165509b5c
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 13:21:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ bot.on('ready', () => {
     bot.user.setStatus('available')
     bot.user.setPresence({
         game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
+            name: 'Mokry Kamilek <3',
+            type: "WATCHING",
+            url: "https://www.pornhub.com/"
         }
     });
 });
