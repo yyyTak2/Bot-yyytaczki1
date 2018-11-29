69bc200facbc1b2a30e12795940e59e265e5994a
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 14:47:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,42 +15,9 @@ bot.on('ready', () => {
     });
 });
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
-
+client.on('message', message => {
+else if(message.content.toLowerCase() === 'lol')
+      message.channel.send('eksdi');
+   
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.B0T_T0KEN);
