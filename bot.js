51eea943efc9eada45800a193aebc509aa9a07f6
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 12:50:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-client.on('ready', () => { client.user.setGame('GAME HERE') })
+client.user.setActivity('YouTube', { type: 'WATCHING' });
     
 });
 
