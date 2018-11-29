173647753aecf831337f1261cbb0b0c5a6baab54
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 13:02:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("Ready");
-  client.user.setActivity('YouTube', { type: 'WATCHING' });
+  client.user.setActivity('YouTube', { type: 'WATCHING' })
 
 client.login(process.env.B0T_T0KEN);
