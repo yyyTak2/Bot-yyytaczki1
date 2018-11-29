78c148143e2902cf30e7e55ae802f1af4f1764e3
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-29 13:38:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,12 @@ var Discord = require('discord.js');
 var bot = new Discord.Client();
 
 bot.on('ready', () => {
-    bot.user.setStatus('available')
+    bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
             name: 'Mokry Kamilek',
-            type: "WATCHING",
+            type: "STREAMING",
+            url: "Mokry Kamilek"
         }
     });
 });
