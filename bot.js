7d6564080e172853f735059b7a6680a2162765ab
Dominik <dominik.cz89.com@gmail.com>
2019-03-11 13:30:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
-            name: '❗ ZAREJESTRUJ ❗',
+            name: '❗ ZAREJESTRUJ SIĘ ❗',
             type: "STREAMING",
             url: "https://www.twitch.tv/yyytak32"
             
